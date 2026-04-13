c1016744f3174092afcdb93dd76ea214548c4687
Brody Wingrove <88740722+BroD54@users.noreply.github.com>
2026-04-13 10:06:11 -0600

    Added IsExternalInit so record works

--- Assets/Scripts/Core/Events/GrandTichuDecisionNeededEvent.cs
+++ Assets/Scripts/Core/Events/GrandTichuDecisionNeededEvent.cs
@@ -1,3 +1,8 @@
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
+
 namespace Core.Events
 {
     public record GrandTichuDecisionNeededEvent(string PlayerName, int PlayerIndex);
